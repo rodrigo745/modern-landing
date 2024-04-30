b6c884a0a709b5fd6d857610ff652c338b69d17d
rodrigo745 <roodrigoquinteros@gmail.com>
2024-04-30 15:13:24 -0300

    main

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import NavBar from "@/components/navBar/navBar";
 import Header from "@/components/header/header";
+import Main from "@/components/main/main";
 import Esferas from "@/components/esferas";
 
 export default function Home() {
@@ -9,8 +10,15 @@ export default function Home() {
       <Esferas lado="left-0 z-0"/>
       <NavBar/>      
       <Header/>
-      <Esferas lado="right-20 top-[40vh] z-0"/>
+      <div className="esfera2 absolute right-72 top-[34vh]"></div>
       <Image src="/empresas.svg" width={600} height={200} alt="empresas" className="mt-16 md:mt-32"/>
+      <div className="overflow-hidden w-full h-full z-0">
+        <div className="esfera2 absolute right-0 "></div>
+      </div>
+      <Main/>
+      <div className="overflow-hidden w-full h-full z-0">
+        <div className="esfera3 absolute right-0 "></div>
+      </div>
     </main>
   );
 }
